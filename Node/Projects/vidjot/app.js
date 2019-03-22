2822832bf7b7efc1bd0471351fce181ca8a55d68
rickytorrez <torrez.ricky@ymail.com>
2019-03-22 12:31:12 -0400

    express session and flash msg

--- Node/Projects/vidjot/app.js
+++ Node/Projects/vidjot/app.js
@@ -4,6 +4,15 @@ const express = require('express');
 // bring in handlebars
 const exphbs = require('express-handlebars');
 
+// bring in method override
+const methodOverride = require('method-override');
+
+// bring in flash
+const flash = require('connect-flash');
+
+// bring in session
+const session = require('express-session');
+
 // bring in body-parser
 const bodyParser = require('body-parser');
 
@@ -33,6 +42,31 @@ app.engine('handlebars', exphbs({
 }));
 app.set('view engine', 'handlebars');
 
+// body parser middleware
+app.use(bodyParser.urlencoded({ extended: false }))
+app.use(bodyParser.json())
+
+// method-override middleware
+app.use(methodOverride('_method'));
+
+// express-session middleware
+app.use(session ({
+    secret: 'keyboard cat',
+    resave: true,
+    saveUninitialized: true
+}))
+
+// flash middleware
+app.use(flash());
+
+// global variables 
+app.use(function(req, res, next){
+    res.locals.success_msg = req.flash('success_msg');
+    res.locals.error_msg = req.flash('error_msg');
+    res.locals.error = req.flash('error');
+    next();
+});
+
 // index route, renders index handlebar, we passed the variable title to the views
 // the parameter is passed by adding a variable of title on the render function
 app.get('/', (req, res) => {
@@ -47,48 +81,103 @@ app.get('/about', (req, res) => {
     res.render('about');
 })
 
-// body parser middleware
-app.use(bodyParser.urlencoded({ extended: false }))
-app.use(bodyParser.json())
+// idea index route
+app.get('/ideas', (req, res)=> {
+
+// fetch the ideas from the db and pass them to the render
+// finds all the ideas
+    Idea.find({})
+
+// sorts them by date, descending
+        .sort({date:'desc'})
+
+// return the ideas to the index
+        .then(ideas => {
+            res.render('ideas/index', {
+                ideas:ideas
+            });
+        });
+})
 
 // add idea route 
 app.get('/ideas/add', (req, res) => {
     res.render('ideas/add');
 })
 
-// process form - post request
-// validations 
-app.post('/ideas', (req, res)=> {
-    let errors = [];
-    if(!req.body.title) {
-        errors.push({text: 'Please add a title'})
-    }
-    if(!req.body.details) {
-        errors.push({text: 'Please add some details'})
-    }
+// edit idea route 
+app.get('/ideas/edit/:id', (req, res) => {
+// find one specific idea instead of the array
+    Idea.findOne({
 
-    if(errors.length > 0) {
+// match the _id with whatever is in the url
+        _id: req.params.id
+    })
+
+// promise to get the single idea and render by passing it in as a parameter
+    .then(idea => {
+        res.render('ideas/edit', {
+            idea:idea
+        });
+    });
+});
+
+// post process 
+app.post('/ideas', (req, res) => {
+    let errors = [];
+  
+    if(!req.body.title){
+        errors.push({text:'Please add a title'});
+    }
+    if(!req.body.details){
+        errors.push({text:'Please add some details'});
+    }
+  
+    if(errors.length > 0){
         res.render('ideas/add', {
             errors: errors,
             title: req.body.title,
             details: req.body.details
         });
-
-// after validation passes, add the idea to the DB
     } else {
         const newUser = {
             title: req.body.title,
             details: req.body.details
         }
-
-// Use a promise to return the idea and redirect to the ideas listing page
         new Idea(newUser)
             .save()
             .then(idea => {
+                req.flash('success_msg', 'Video idea added');
+                res.redirect('/ideas');
+        })
+    }
+});
+
+// edit form process
+app.put('/ideas/:id', (req, res)=> {
+    Idea.findOne({
+        _id: req.params.id
+    })
+    .then(idea => {
+// new values
+        idea.title = req.body.title;
+        idea.details = req.body.details;
+
+        idea.save()
+            .then(idea => {
+                req.flash('success_msg', 'Video idea updated');
                 res.redirect('/ideas');
             })
-    }
-})
+    })
+});
+
+// delete idea
+app.delete('/ideas/:id', (req,res) => {
+    Idea.remove({_id: req.params.id})
+        .then(() => {
+            req.flash('error_msg', 'Video idea removed');
+            res.redirect('/ideas');
+        })
+});
 
 // create a variable for the port
 const port = 5000;
