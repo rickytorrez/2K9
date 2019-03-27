739a508e44d98d4aa212d5b9da0b82a58489bbf5
rickytorrez <torrez.ricky@ymail.com>
2019-03-27 00:07:38 -0400

    database fix

--- Node/Projects/vidjot/config/database.js
+++ Node/Projects/vidjot/config/database.js
@@ -1,5 +1,5 @@
 if (process.env.NODE_ENV === 'production'){
-    module.exports = {mongoURI: 'mongodb://<rick>:<rick1234>@ds255767.mlab.com:55767/vidjot-prod'}
+    module.exports = {mongoURI: 'mongodb://rick:test1234@ds125616.mlab.com:25616/vidjot-prod'}
 } else {
     module.exports = {mongoURI: 'mongodb://localhost/vidjot-dev'}
 }
