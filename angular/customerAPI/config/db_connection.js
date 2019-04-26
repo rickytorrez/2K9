2b6a165a3701c3aa4dad1ef092e633fe970d0831
rickytorrez <torrez.ricky@ymail.com>
2019-04-26 12:42:11 -0400

    cutomer api

---
+++ angular/customerAPI/config/db_connection.js
@@ -0,0 +1,12 @@
+var dbProps = require("./db_properties");
+var mysql = require("mysql");
+
+module.exports = {
+    getDbConnection: ()=>{
+        return mysql.createConnection ({
+            host: dbProps.host,
+            user: dbProps.user,
+            password: dbProps.password,
+            database: dbProps.dbName
+    })}
+}
---
+++ angular/customerAPI/config/db_properties.js
@@ -0,0 +1,6 @@
+module.exports = {
+    host: "locahost",
+    user: "root",
+    password: "root",
+    dbName: "042519_customerAPI"
+}
