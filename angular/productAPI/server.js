63a3c81cca9d1b61435db7cdc0f9d967cee8d8a1
rickytorrez <torrez.ricky@ymail.com>
2019-04-29 12:23:41 -0400

    angular 6

--- angular/productAPI/server.js
+++ angular/productAPI/server.js
@@ -10,6 +10,14 @@ app.use(bodyparser.urlencoded({extended:false}));
 var productAPI = require("./controllers/product.controller");
 app.use("/api/products", productAPI)
 
+/* cors and expressjs */
+app.use(function(req,res,next){
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Methods","Origin, X-Requested-With, Content-Type, Accept")
+    res.header("Access-Control-Allow-Methods","GET,POST,PUT,DELETE")
+    next();
+})
+
 /** configure the port */
 app.listen(8080);
 console.log("Server running on 8080")
