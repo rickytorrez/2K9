24f8a315ed0f3845677e8ab5953a4a7024252c72
rickytorrez <torrez.ricky@ymail.com>
2019-04-29 14:35:11 -0400

    path variable

--- java/microservices/restful-web-services/src/main/java/com/ericardo/webservices/restfulwebservices/helloWorldController.java
+++ java/microservices/restful-web-services/src/main/java/com/ericardo/webservices/restfulwebservices/helloWorldController.java
@@ -1,12 +1,14 @@
 package com.ericardo.webservices.restfulwebservices;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 //Controller
 @RestController
 public class helloWorldController {
 
+	// returns helloWorld
 	@GetMapping(path="/hello-world")
 	public String helloWorld() {
 		return "Hello World";
@@ -17,5 +19,11 @@ public class helloWorldController {
 	public HelloWorldBean helloWorldBean() {
 		System.out.println("message");
 		return new HelloWorldBean ("Hello World");
+	}
+	
+	// returns a string with a path variable value
+	@GetMapping(path="/hello-world/path-variable/{name}")
+	public HelloWorldBean helloWorldPathVariable(@PathVariable String name) {
+		return new HelloWorldBean (String.format("Hello World %s", name));
 	}
 }
