058cdcc98976e26979fbcfb1e1719fe5aafd63df
rickytorrez <torrez.ricky@ymail.com>
2019-04-30 14:14:25 -0400

    todo class in component.ts

--- fstack/todo/frontend/todo/src/app/app-routing.module.ts
+++ fstack/todo/frontend/todo/src/app/app-routing.module.ts
@@ -3,12 +3,15 @@ import { Routes, RouterModule } from '@angular/router';
 import { LoginComponent } from './components/login/login.component';
 import { WelcomeComponent } from './components/welcome/welcome.component';
 import { ErrorComponent } from './components/error/error.component';
+import { ListTodosComponent } from './components/list-todos/list-todos.component';
 
 //welcome 
 const routes: Routes = [
     { path:'', component: LoginComponent },
     { path:'login', component: LoginComponent },
     { path:'welcome/:name', component: WelcomeComponent },
+    { path:'todos', component: ListTodosComponent },
+
 
     // Every other path is defined, lead to the error component if client hits an unknown route
     { path:'**', component: ErrorComponent },
--- fstack/todo/frontend/todo/src/app/components/list-todos/list-todos.component.ts
+++ fstack/todo/frontend/todo/src/app/components/list-todos/list-todos.component.ts
@@ -1,11 +1,28 @@
 import { Component, OnInit } from '@angular/core';
 
+export class Todo{
+  constructor(
+    public id: number,
+    public description: string,
+    public done: boolean,
+    public targetDate: Date
+  ){
+    
+  }
+}
+
 @Component({
   selector: 'app-list-todos',
   templateUrl: './list-todos.component.html',
   styleUrls: ['./list-todos.component.css']
 })
 export class ListTodosComponent implements OnInit {
+
+  todos = [
+    new Todo(1, 'Take a nap', false, new Date()),
+    new Todo(2, 'Get some lunch', false, new Date()),
+    new Todo(2, 'Go to work', false, new Date())
+  ]
 
   constructor() { }
 
