c12466c2b293309aeab0f226b98d69dfabf4f0cd
rickytorrez <torrez.ricky@ymail.com>
2019-05-01 14:42:34 -0400

    cross origin, observables and subscribe

--- fstack/todo/backend/todofullstack/src/main/java/com/ericardo/todofullstack/HelloWorldController.java
+++ fstack/todo/backend/todofullstack/src/main/java/com/ericardo/todofullstack/HelloWorldController.java
@@ -1,10 +1,12 @@
 package com.ericardo.todofullstack;
 
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 // controller
+@CrossOrigin(origins="http://localhost:4200")
 @RestController
 public class HelloWorldController {
 
--- fstack/todo/frontend/todo/src/app/components/welcome/welcome.component.ts
+++ fstack/todo/frontend/todo/src/app/components/welcome/welcome.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
+import { WelcomeDataService } from 'src/app/services/data/welcome-data.service';
 
 @Component({
   selector: 'app-welcome',
@@ -11,11 +12,18 @@ export class WelcomeComponent implements OnInit {
   name = '';
 
   // dependency injection to pass a parameter
-  constructor(private _route:ActivatedRoute) { }
+  constructor(
+    private _route:ActivatedRoute,
+    private _welcomeService:WelcomeDataService) { }
 
   ngOnInit() {
     // assigns the parameter passed as name to our variable name in this component
     this.name = this._route.snapshot.params['name']
+  }
+
+  getWelcomeMessage(){
+    console.log(this._welcomeService.executeHelloWorldBeanService());
+    this._welcomeService.executeHelloWorldBeanService().subscribe();
   }
 
 }
