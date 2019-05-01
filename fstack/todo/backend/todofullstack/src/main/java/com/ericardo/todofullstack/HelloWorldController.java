d4932c6379c4c86f1a8941c8038ec98d798d3b46
rickytorrez <torrez.ricky@ymail.com>
2019-05-01 13:54:35 -0400

    backend

---
+++ fstack/todo/backend/todofullstack/src/main/java/com/ericardo/todofullstack/HelloWorldController.java
@@ -0,0 +1,28 @@
+package com.ericardo.todofullstack;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+// controller
+@RestController
+public class HelloWorldController {
+
+	// @RequestMapping(method=RequestMethod.GET, path="/hello-world")
+	@GetMapping(path="/hello-world")
+	public String helloWorld() {
+		return "hello world";
+	}
+	
+	// hello-world-bean
+	@GetMapping(path="/hello-world-bean")
+	public HelloWorldBean helloWorldBean() {
+		return new HelloWorldBean ("hello world");
+	}
+	
+	// hello-world-bean/path-variable/ricky
+	@GetMapping(path="/hello-world-bean/path-variable/{name}")
+	public HelloWorldBean helloWorldPathVariable(@PathVariable String name) {
+		return new HelloWorldBean (String.format("Hello world, %s", name));
+	}
+}
---
+++ fstack/todo/backend/todofullstack/src/main/java/com/ericardo/todofullstack/TodofullstackApplication.java
@@ -0,0 +1,13 @@
+package com.ericardo.todofullstack;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TodofullstackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TodofullstackApplication.class, args);
+	}
+
+}
